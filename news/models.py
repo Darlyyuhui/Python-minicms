10e70fa9dcaf902e8f6b2b72192894f27a1accef
Rich.Healthy <darlyyuhui@hotmail.com>
2016-09-08 14:36:52 +0800

    init

---
+++ news/models.py
@@ -0,0 +1,74 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.sitemaps import ping_google
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from DjangoUeditor.models import UEditorField
+
+
+@python_2_unicode_compatible
+class Column(models.Model):
+    name = models.CharField(u'栏目名称',max_length=256)
+    slug = models.CharField(u'栏目网址',max_length=256,db_index=True)
+    intro = models.TextField(u'栏目简介',default='')
+
+    #添加导航
+    nav_display = models.BooleanField(u'导航显示',default=False)
+    home_display = models.BooleanField(u'首页显示',default=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('column',args=(self.slug,))
+
+    class Meta:
+        verbose_name = '栏目'
+        verbose_name_plural = '栏目'
+        ordering = ['name'] #按照哪个栏目排序
+
+@python_2_unicode_compatible
+class Article(models.Model):
+    column = models.ManyToManyField(Column,verbose_name='归属栏目')
+
+    title = models.CharField(u'标题',max_length=256)
+    slug = models.CharField(u'网址',max_length=256)
+
+    author = models.ForeignKey('auth.User',blank=True,null=True,verbose_name='作者')
+    content = UEditorField(u'内容',height=300,width=1000,
+                           default=u'',blank=True,imagePath="uploads/images/",
+                           toolbars='besttome',filePath='uploads/files/')
+        #models.TextField(u'内容',default='',blank=True)
+
+    published = models.BooleanField(u'正式发布',default=True)
+
+    pub_date = models.DateTimeField(u'发表时间',auto_now_add=True,editable=True)
+    update_time = models.DateTimeField(u'更新时间',auto_now=True,null=True)
+
+    def __str__(self):
+        return  self.title
+
+    def get_absolute_url(self):
+        return reverse('article',args=(self.pk,self.slug,))
+
+        # ...
+
+    def save(self, *args, **kwargs):
+        super(Article, self).save(*args, **kwargs)
+        try:
+            ping_google()
+        except Exception:
+            # Bare 'except' because we could get a variety
+            # of HTTP-related exceptions.
+            pass
+
+    class Meta:
+        verbose_name = '教程'
+        verbose_name_plural = '教程'
+
+
+
+
+
